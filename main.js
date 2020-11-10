0d111ace96b490408db5c5eac0d758c665336f57
Jony Talukdar <jonytalukdar30@gmail.com>
2020-11-09 22:26:48 -0800

    i have added  javascript basic

---
+++ main.js
@@ -0,0 +1,18 @@
+// basic of javascript
+
+// console.log('hello')
+// console.log('hello somebody here')
+
+// var hello = 'something'
+// console.log(hello)
+
+// var someNumber = 12
+// console.log(someNumber)
+
+// document.getElementById('heading').innerHTML = 'this is demo for test'
+// document.getElementById('test').innerHTML = 'this is so aweome'
+
+// document.querySelector('h1').innerHTML = 'this is heading one'
+
+// var age = prompt('what is your age ??')
+// document.getElementById('age').innerHTML = age
